68d6a929f93501196d31367f579102a589f9a735
nktchen <nktchen.dev@gmail.com>
2025-02-11 06:22:19 +0300

    created logic to write projects and images to database

--- api/app.js
+++ api/app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const CORS = require("cors");
-// const fs = require("fs");
-const mysql = require("mysql2");
-// const multer = require("multer");
+const mysql = require("mysql2/promise");
+const multer = require("multer");
+const { randomUUID } = require("crypto");
 
 const app = express();
 const port = 5000;
 
 app.use(CORS());
-app.use(express.json()); // Middleware для обработки JSON
-app.use("/images", express.static(__dirname + "/img"));
+app.use(express.json()); // обработка JSON
+app.use("/images", express.static(__dirname + "/img")); // обработка статических файлов
 const pool = mysql.createPool({
+  // база данных
   connectionLimit: 5,
   host: "localhost",
   user: "root",
@@ -18,20 +19,21 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
 });
 
-//
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "img");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+  // обработка загрузки файлов
+  destination: function (req, file, cb) {
+    cb(null, "img");
+  },
+  filename: function (req, file, cb) {
+    let filenameExtension = file.originalname.split(".").at(-1);
+    cb(null, `${randomUUID()}.${filenameExtension}`);
+  },
+});
+const upload = multer({ storage: storage });
 
-/**
+/** returns JSON with all projects
  * GET : /api/projects
- * returns JSON with all projects: {
+ * response json: {
  *   [
  *     {
  *       "id" : int
@@ -46,7 +48,7 @@ const pool = mysql.createPool({
  * responses :
  *  200 - successful operation
  *  204 - no content
- */
+ **/
 app.get("/api/projects", (req, res) => {
   pool.query("SELECT * FROM projects", (err, result) => {
     if (err) return console.log(err);
@@ -63,18 +65,45 @@ app.get("/api/projects", (req, res) => {
   //   res.json(); // Отправляем массив путей к файлам в формате JSON
   // });
 });
-/**
+
+/** adds project
  * POST : /api/projects
  * gets HTML form with enctype="multipart/form-data"
- * names - title, description, date, images
+ * names - title, description
  * returns HTTP codes
  * parameters -
  * responses
  *  201 - successful operation
  *  400 - bad request!
- */
-app.post("/api/projects", (req, res) => {
-  pool.query("INSERT INTO projects(title, description, date) SET ?", []);
+ *  500 - error while uploading
+ **/
+app.post("/api/projects", upload.any(), (req, res) => {
+  let imgFiles = req.files; //uploading files by multer
+  if (!imgFiles) res.status(400).send("Ошибка при загрузке файлов");
+  let date = new Date();
+  pool
+    .query("INSERT INTO projects(title, description, date) VALUES (?, ?, ?)", [
+      req.body.title,
+      req.body.description,
+      `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${date.getDate()}`,
+    ])
+    .then(([result]) => {
+      console.log(result);
+      const jobID = result.insertId;
+      return pool.execute(
+        "INSERT INTO images(project_id, image_filename) VALUES (?, ?)",
+        [jobID, "images"],
+      );
+    })
+    .then(() => {
+      res.send(`Проект и изображение успешно добавлены`);
+    })
+    .catch((error) => {
+      console.error(error);
+      res
+        .status(500)
+        .send(`Ошибка при добавлении проекта в базу данных: ${error}`);
+    });
 });
 
 app.listen(port, function () {
---
+++ web/src/components/UploadForm/UploadForm.jsx
@@ -0,0 +1,32 @@
+const UploadForm = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+    const fetchURL = "http://127.0.0.1:5000/api/projects";
+    fetch(fetchURL, {
+      method: "POST",
+      body: form,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <form onSubmit={handleSubmit} method="post" encType="multipart/form-data">
+      <label htmlFor="title">Заголовок</label>
+      <input id="title" name="title" type="text" required autoFocus />
+
+      <label htmlFor="description">Описание</label>
+      <input id="description" name="description" type="text" />
+
+      <label htmlFor="image"></label>
+      <input id="image" name="image" type="file" multiple required />
+
+      <button type="submit">Добавить!</button>
+    </form>
+  );
+};
+
+export default UploadForm;
