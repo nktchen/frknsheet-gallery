b26a62aa314a2b347784552a3b23a01311536b07
nktchen <nktchen.dev@gmail.com>
2025-05-06 23:18:09 +0300

    refactor code

--- web/src/components/UploadForm/UploadForm.jsx
+++ web/src/components/UploadForm/UploadForm.jsx
@@ -1,18 +1,19 @@
-const UploadForm = () => {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+const handleSubmit = async (e) => {
+  e.preventDefault();
 
-    const form = new FormData(e.target);
-    const fetchURL = "http://127.0.0.1:5000/api/projects";
-    fetch(fetchURL, {
-      method: "POST",
-      body: form,
+  const form = new FormData(e.target);
+  const fetchURL = "http://127.0.0.1:5000/api/projects";
+  fetch(fetchURL, {
+    method: "POST",
+    body: form,
+  })
+    .then((res) => {
+      console.log(res);
     })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.log(err));
-  };
+    .catch((err) => console.log(err));
+};
+
+const UploadForm = () => {
   return (
     <form onSubmit={handleSubmit} method="post" encType="multipart/form-data">
       <label htmlFor="title">Заголовок</label>
