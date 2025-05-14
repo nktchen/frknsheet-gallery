15eb8f4c1146dacc4aa0b04e2e3bfbb1769125b2
nktchen <nktchen.dev@gmail.com>
2025-05-15 01:33:12 +0300

    add skeleton

--- web/src/components/Project/Project.jsx
+++ web/src/components/Project/Project.jsx
@@ -2,26 +2,29 @@ import styles from "./Project.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import Skeleton from "../assets/Skeleton.jsx";
 
 const Project = () => {
-  const [project, setProject] = useState({}); //TODO add sceleton
+  const [project, setProject] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   let params = useParams();
 
   useEffect(() => {
     axios
       .get(`http://127.0.0.1:5000/api/projects/${params.id}`)
-      .then((res) => res.data)
-      .then((data) => {
-        setProject(data);
+      .then((res) => {
+        setProject(res.data);
+        setIsLoading(false);
       })
       .catch((err) => console.error("Ошибка при получении проекта:", err)); //TODO add Error page
-  });
-  const formattedDate = new Date(project.date).toLocaleDateString();
-  return (
+  }, [params.id]);
+  return isLoading ? (
+    <Skeleton></Skeleton>
+  ) : (
     <article className={styles["project-container"]}>
       <h3>{project.title}</h3>
       <p>{project.description}</p>
-      <p>{formattedDate}</p>
+      <p>{new Date(project.date).toLocaleDateString()}</p>
       {project.imagesUrls.map((url, index) => (
         <img alt={project.description} key={index} src={url} />
       ))}
