d4ed30d2ea156decc4809fbd8f3cb35ec4e325fc
nktchen <chennikita70@gmail.com>
2025-06-22 18:00:29 +0300

    add preview of the downloaded files and add some style for the ui

--- web/src/components/UploadForm/UploadForm.jsx
+++ web/src/components/UploadForm/UploadForm.jsx
@@ -1,10 +1,26 @@
+import style from "./UploadForm.module.css";
 import Popup from "../assets/Popup/Popup.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const UploadForm = () => {
+  const [previews, setPreviews] = useState([]);
   const [isPopup, setIsPopup] = useState(false);
   const [successInfo, setSuccessInfo] = useState(null); // null or {isSuccessfull: boolean, text: string}
+  const now = new Date();
 
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    const newPreviews = files.map((file) => ({
+      file,
+      url: URL.createObjectURL(file),
+    }));
+    setPreviews(newPreviews);
+  };
+  const showPopup = (isSuccessfull, text) => {
+    setSuccessInfo({ isSuccessfull, text });
+    setIsPopup(true);
+    setTimeout(() => setIsPopup(false), 10000);
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     const form = new FormData(e.target);
@@ -15,35 +31,70 @@ const UploadForm = () => {
       body: form,
     })
       .then(async (res) => {
-        setSuccessInfo({ isSuccessfull: res.ok, text: await res.text() });
-        setIsPopup(true);
+        showPopup(res.ok, await res.text());
       })
       .catch((err) => {
-        setIsPopup(true);
-        setSuccessInfo({ isSuccessfull: false, text: err });
+        showPopup(false, err.message || "Ошибка загрузки");
       });
   };
+  useEffect(() => {
+    return () => {
+      previews.forEach((p) => URL.revokeObjectURL(p.url)); // удаляем URLs от image preview
+    };
+  }, [previews]);
 
   return (
-    <form onSubmit={handleSubmit} method="post" encType="multipart/form-data">
-      <label htmlFor="title">Заголовок</label>
-      <input id="title" name="title" type="text" required autoFocus />
-
-      <label htmlFor="description">Описание</label>
-      <input id="description" name="description" type="text" />
-
-      <label htmlFor="image"></label>
-      <input id="image" name="image" type="file" multiple required />
-
-      <button type="submit">Добавить!</button>
-
-      {isPopup && (
-        <Popup
-          isSuccessfull={successInfo.isSuccessfull}
-          text={successInfo.text}
+    <section className={style.projectWrapper}>
+      <div className={style.imgWrapper}>
+        {previews.map((preview, index) => (
+          <img key={index} src={preview.url} alt={`preview ${index}`} />
+        ))}
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        method="post"
+        encType="multipart/form-data"
+        className={style.infoWrapper}
+      >
+        <input
+          className={style.infoTitle}
+          name="title"
+          type="text"
+          placeholder="Заголовок"
+          required
+          autoFocus
         />
-      )}
-    </form>
+        <p className="infoDate">
+          {now.toLocaleDateString("ru-RU", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+          })}
+        </p>
+        <input
+          className={style.infoDescription}
+          name="description"
+          type="text"
+          placeholder="Описание"
+        />
+        <input
+          id="image"
+          name="image"
+          type="file"
+          multiple
+          required
+          onChange={handleImageChange}
+        />
+        <button type="submit">Добавить!</button>{" "}
+        {/* <- disabled while uploading*/}
+        {isPopup && (
+          <Popup
+            isSuccessfull={successInfo.isSuccessfull}
+            text={successInfo.text}
+          />
+        )}
+      </form>
+    </section>
   );
 };
 
